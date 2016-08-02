afd19d544c376b5f2a028af26ec0ff0bf66a7bc3
Ryota Konno <r.konno@nowall.co.jp>
2016-08-02 22:15:47 +0900

    [add] template file was available

--- main.go
+++ main.go
@@ -3,22 +3,30 @@ package main
 import (
     "log"
     "net/http"
+    "text/template"
+    "path/filepath"
+    "sync"
 )
 
-func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte(`
-            <html>
-                <head>
-                    <title>チャット</title>
-                </head>
-                <body>
-                    チャットしましょう！
-                </body>
-            </html>
-        `))
-    })
+type templateHandler struct {
+    once        sync.Once
+    filename    string
+    temp1       *template.Template  // 1つのテンプレート
+}
 
+// ServeHTTP: HTTPリクエストを処理する
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    t.once.Do(func() {
+        t.temp1 =
+            template.Must(template.ParseFiles(filepath.Join("templates",
+                t.filename)))
+    })
+    t.temp1.Execute(w, nil)
+}
+
+func main() {
+    // ルート
+    http.Handle("/", &templateHandler{filename: "chat.html"})
     // Webサーバーを開始します
     if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal("ListenAddServe:", err)
